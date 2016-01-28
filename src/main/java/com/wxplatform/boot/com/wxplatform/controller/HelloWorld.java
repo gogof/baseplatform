2306568121d2e60f23bc427783328337f542c862
fanxx <fxx_7788@163.com>
2016-01-28 11:34:45 +0800

    微信开发平台 同步git 测试

--- src/main/java/com/wxplatform/boot/com/wxplatform/controller/HelloWorld.java
+++ src/main/java/com/wxplatform/boot/com/wxplatform/controller/HelloWorld.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorld {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello(){
         return "hello world!";
     }
